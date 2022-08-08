760f9302db4f6de01813b2adb5b4745a05385c94
Mike Bregoli <briggleslol@gmail.com>
2022-08-08 17:11:28 -0400

    Added event listener to each box to put in a new value when clicked, then redraw the board.

--- javascript.js
+++ javascript.js
@@ -3,20 +3,39 @@ let gameBoard = (function() {
     ['X', 'X', 'O'],
     ['O', 'X', 'X'],
     ['O', 'O', 'X']];
+    let ticTacContainer = document.getElementById('gameContainer');
+
+    const placeMarker = (x, y, value) => {
+        console.log(gameBoardArray[x][y]);
+        gameBoardArray[x][y] = value;
+        drawBoard();
+    }
+
+    const clearDisplay = (display) => {
+        while (ticTacContainer.firstChild) {
+          ticTacContainer.removeChild(ticTacContainer.lastChild);
+        }}
+
 
     const drawBoard = () => {
-       let ticTacContainer = document.getElementById('gameContainer');
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 3; j++) {
+        clearDisplay(ticTacContainer);
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
                 let ticTacBox = document.createElement('div');
                 ticTacBox.className = 'ticTacBox';
+                ticTacBox.id = 'row' + i + 'col' + j;
+                console.log(gameBoardArray[i][j]);
                 ticTacBox.textContent = gameBoardArray[i][j];
-//                console.log(ticTacContainer.textContent);
-  //              console.log(gameBoardArray[i][j]);
+                ticTacBox.addEventListener('click', () => {
+                    console.log(i);
+                    placeMarker(i, j, 'poop');
+                });
+
                 ticTacContainer.appendChild(ticTacBox);
             }
         }
     }
+
     return {
         drawBoard,
     };
